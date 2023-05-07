90e4f31f9375b668fd72e9b984aa5cdd509d56eb
Anette-Gallegos <agallegosco@unsa.edu.pe>
2023-05-07 16:00:51 -0500

    Agregando Scanner a HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,13 @@
 package sorteo;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 		System.out.println("Hola Mundo");
 		System.out.println("Otro hola mundo");
+        System.out.println("¡Hola Mundo y a " + sc.nextLine() + "!");
 	}
 }
