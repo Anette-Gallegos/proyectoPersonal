99798432ae3d0de06ab5aed149893d4aa318bbab
Anette-Gallegos <agallegosco@unsa.edu.pe>
2023-05-07 16:10:12 -0500

    Agregando saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,7 +7,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		System.out.println("Hola Mundo");
-		System.out.println("Otro hola mundo");
+		System.out.println("Bienvenido al curso");
         System.out.println("¡Hola Mundo y a " + sc.nextLine() + "!");
         System.out.println("Espero que estes bien");
 	}
